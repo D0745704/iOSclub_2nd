8a39d84647948c699395c683bcd4345728445e29
D0745704 <mushroom512348@gmail.com>
2021-10-19 18:38:04 +0800

    Initial Commit

---
+++ iOSclub_2nd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSclub_2nd
+//
+//  Created by 仲輝 on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSclub_2nd/iOSclub_2ndApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSclub_2ndApp.swift
+//  iOSclub_2nd
+//
+//  Created by 仲輝 on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct iOSclub_2ndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
